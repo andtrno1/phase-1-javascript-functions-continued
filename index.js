6fbc75c5b65f46424589308734a46b3fba1c5e77
andtrno1 <andtrno1@gmail.ccom>
2022-03-19 16:56:23 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+ function saturdayFun(activity = "roller-skate"){
+    var printActivity = `This Saturday, I want to ${activity}!`;
+    return printActivity;
+
+}
+
+function mondayWork(activity = "go to the office"){
+    var printActivity = `This Monday, I will ${activity}.`;
+    return printActivity;
+
+}
+
+function wrapAdjective(input ="*") {
+    return function(adj="special") {
+      return `You are ${input}${adj}${input}!`;
+}
+}
